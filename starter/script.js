6fa5743e05f76a574db3fc444b16bdef22c1c4c1
Svetozar Stanev <svetozar.stanev@gmail.com>
2022-11-06 20:45:51 +0200

    completed

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,8 @@
+console.log("Script started");
+const addToDoBtn = document.getElementById("add-to-do-btn");
+const toDoInput = document.getElementsByTagName("input")[0];
+const toDoContainer = document.querySelector("#to-do-container");
+
 function preserveToDos() {
   const allToDos = Array.from(
     document.querySelectorAll(".to-do-instance p")
@@ -8,8 +13,45 @@ function preserveToDos() {
 function loadToDos() {
   const loadedToDos = window.sessionStorage.getItem("todos").split(",");
   if (loadedToDos && loadedToDos.length > 0) {
+    console.log(loadedToDos);
     loadedToDos.forEach((toDo) => {
-      toDoContainer.appendChild(createToDoElement(toDo));
+      toDoContainer.appendChild(createToDo(toDo));
     });
   }
 }
+
+function createToDo(text) {
+  const divElement = document.createElement("div");
+  divElement.classList.add("to-do-instance");
+
+  const pElement = document.createElement("p");
+  pElement.textContent = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.addEventListener("click", () => {
+    divElement.remove();
+    preserveToDos();
+  });
+
+  divElement.appendChild(pElement);
+  divElement.appendChild(deleteBtn);
+
+  return divElement;
+}
+
+function addToDo() {
+  const validText = toDoInput.value.trim();
+  if (validText) {
+    const newToDo = createToDo(validText);
+    toDoContainer.appendChild(newToDo);
+    toDoInput.value = "";
+    preserveToDos();
+  } else {
+    alert("Please type in some text");
+  }
+}
+
+addToDoBtn.addEventListener("click", addToDo);
+
+loadToDos();
